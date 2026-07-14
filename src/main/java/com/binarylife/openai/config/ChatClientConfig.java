cad031dd85950773c68bb8d58cbe309cd9aa3ff7
Aritra Chatterjee <aritrachatterjee2007@gmail.com>
2026-07-14 12:10:58 +0530

    LLM006-provide default system and user messages

--- src/main/java/com/binarylife/openai/config/ChatClientConfig.java
+++ src/main/java/com/binarylife/openai/config/ChatClientConfig.java
@@ -2,23 +2,27 @@ package com.binarylife.openai.config;
 
 import org.springframework.ai.chat.client.ChatClient;
 //import org.springframework.ai.ollama.OllamaChatModel;
-import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
-//public class ChatClientConfig {
-//
-//    @Bean
-//    public ChatClient openAiChatClient(OpenAiChatModel openAiChatModel) {
-//        return ChatClient.create(openAiChatModel);
-//    }
-//
-//    @Bean
-//    public ChatClient ollamaChatClient(OllamaChatModel ollamaChatModel
-//    ) {
-//        ChatClient.Builder chatClientBuilder = ChatClient.builder(ollamaChatModel);
-//        return chatClientBuilder.build();
-//    }
-//
-//}
+@Configuration
+public class ChatClientConfig {
+
+    @Bean
+    public ChatClient openAiChatClient(ChatClient.Builder chatClientBuilder) {
+        return chatClientBuilder
+                .defaultSystem("""
+                        You are an internal HR assistant. Your role is to help\s
+                        employees with questions related to HR policies, such as \s
+                        leave policies, working hours, benefits, and code of conduct.
+                        If a user asks for help with anything outside of these topics,\s
+                        kindly inform them that you can only assist with queries related to\s
+                        HR policies.
+                        """)
+                .defaultUser("""
+                        How can you help me?
+                        """)
+                .build();
+    }
+
+}
--- src/main/java/com/binarylife/openai/controller/ChatController.java
+++ src/main/java/com/binarylife/openai/controller/ChatController.java
@@ -11,13 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChatController {
     private final ChatClient chatClient;
 
-    public ChatController(ChatClient.Builder chatClientBuilder){
-        this.chatClient = chatClientBuilder.build();
+    public ChatController(ChatClient chatClient) {
+        this.chatClient = chatClient;
     }
 
     @GetMapping("/chat")
     public String chat(@RequestParam("message") String message) {
         return chatClient.prompt()
+                //overriding default system message
                 .system("""
                         You are an internal IT helpdesk assistant. Your role is to assist\s
                                                 employees with IT-related issues such as resetting passwords,\s

7039ccf3b9bc6dbb2f96841a768732a18ad1f65c
Aritra Chatterjee <aritrachatterjee2007@gmail.com>
2026-07-14 10:55:26 +0530

    LLM004-run multiple chat clients pointing to llama and openai

--- src/main/java/com/binarylife/openai/controller/ChatController.java
+++ src/main/java/com/binarylife/openai/controller/ChatController.java
@@ -6,17 +6,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/api")
+//@RestController
+//@RequestMapping("/api")
 public class ChatController {
-    private final ChatClient chatClient;
-
-    public ChatController(ChatClient.Builder chatClientBuilder){
-        this.chatClient = chatClientBuilder.build();
-    }
-
-    @GetMapping("/chat")
-    public String chat(@RequestParam("message") String message) {
-        return chatClient.prompt(message).call().content();
-    }
+//    private final ChatClient chatClient;
+//
+//    public ChatController(ChatClient.Builder chatClientBuilder){
+//        this.chatClient = chatClientBuilder.build();
+//    }
+//
+//    @GetMapping("/chat")
+//    public String chat(@RequestParam("message") String message) {
+//        return chatClient.prompt(message).call().content();
+//    }
 }

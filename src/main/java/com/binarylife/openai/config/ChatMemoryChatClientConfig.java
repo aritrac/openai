d702f4067c0a58c2faad5d6a507111dcfedd16bc
Aritra Chatterjee <aritrachatterjee2007@gmail.com>
2026-07-23 09:15:35 +0530

    LLM025- implement retrievalAugmentaionAdvisor

--- src/main/java/com/binarylife/openai/config/ChatMemoryChatClientConfig.java
+++ src/main/java/com/binarylife/openai/config/ChatMemoryChatClientConfig.java
@@ -11,6 +11,9 @@ import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.ai.rag.advisor.RetrievalAugmentationAdvisor;
+import org.springframework.ai.rag.retrieval.search.VectorStoreDocumentRetriever;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -26,13 +29,22 @@ public class ChatMemoryChatClientConfig {
     }
 
     @Bean
-    public ChatClient chatMemoryChatClient(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory) {
+    public ChatClient chatMemoryChatClient(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory,
+                                           RetrievalAugmentationAdvisor retrievalAugmentationAdvisor) {
         Advisor loggerAdvisor = new SimpleLoggerAdvisor();
         Advisor tokenAuditAdvisor = new TokenUsageAuditAdvisor();
         Advisor memoryAdvisor = MessageChatMemoryAdvisor.builder(chatMemory).build();
         return chatClientBuilder
-                .defaultAdvisors(List.of(loggerAdvisor, memoryAdvisor, tokenAuditAdvisor))
+                .defaultAdvisors(List.of(loggerAdvisor, memoryAdvisor, tokenAuditAdvisor, retrievalAugmentationAdvisor))
                 .build();
+    }
+
+    @Bean
+    RetrievalAugmentationAdvisor retrievalAugmentationAdvisor(VectorStore vectorStore) {
+        return RetrievalAugmentationAdvisor.builder().documentRetriever(
+                VectorStoreDocumentRetriever.builder().vectorStore(vectorStore)
+                        .topK(3).similarityThreshold(0.5).build()
+        ).build();
     }
 
 }
--- src/main/java/com/binarylife/openai/controller/RAGController.java
+++ src/main/java/com/binarylife/openai/controller/RAGController.java
@@ -31,18 +31,20 @@ public class RAGController {
         this.chatClient = chatClient;
         this.vectorStore = vectorStore;
     }
+
     @GetMapping("/random/chat")
     public ResponseEntity<String> randomChat(@RequestHeader("username") String username, @RequestParam("message") String message) {
-        SearchRequest searchRequest = SearchRequest.builder().query(message).topK(3).similarityThreshold(0.5).build();
-        List<Document> similarDocs = vectorStore.similaritySearch(searchRequest);
+//        SearchRequest searchRequest = SearchRequest.builder().query(message).topK(3).similarityThreshold(0.5).build();
+//        List<Document> similarDocs = vectorStore.similaritySearch(searchRequest);
+//
+//        String similarContext = similarDocs.stream()
+//                .map(Document::getText)
+//                .collect(Collectors.joining(System.lineSeparator()));
 
-        String similarContext = similarDocs.stream()
-                .map(Document::getText)
-                .collect(Collectors.joining(System.lineSeparator()));
-
-        String answer = chatClient.prompt().system(
-                        promptSystemSpec -> promptSystemSpec.text(promptTemplate)
-                                .param("documents", similarContext))
+        String answer = chatClient.prompt()
+//                .system(
+//                        promptSystemSpec -> promptSystemSpec.text(promptTemplate)
+//                                .param("documents", similarContext))
                 .advisors(a -> a.param(CONVERSATION_ID, username))
                 .user(message)
                 .call().content();
@@ -52,16 +54,18 @@ public class RAGController {
 
     @GetMapping("/document/chat")
     public ResponseEntity<String> documentChat(@RequestHeader("username") String username, @RequestParam("message") String message) {
-        SearchRequest searchRequest = SearchRequest.builder().query(message).topK(3).similarityThreshold(0.5).build();
-        List<Document> similarDocs = vectorStore.similaritySearch(searchRequest);
+        //All these commented line stuff is now handled by the RetrievalAugmentedAdvisor bean
+//        SearchRequest searchRequest = SearchRequest.builder().query(message).topK(3).similarityThreshold(0.5).build();
+//        List<Document> similarDocs = vectorStore.similaritySearch(searchRequest);
+//
+//        String similarContext = similarDocs.stream()
+//                .map(Document::getText)
+//                .collect(Collectors.joining(System.lineSeparator()));
 
-        String similarContext = similarDocs.stream()
-                .map(Document::getText)
-                .collect(Collectors.joining(System.lineSeparator()));
-
-        String answer = chatClient.prompt().system(
-                        promptSystemSpec -> promptSystemSpec.text(hrSystemTemplate)
-                                .param("documents", similarContext))
+        String answer = chatClient.prompt()
+//                .system(
+//                        promptSystemSpec -> promptSystemSpec.text(hrSystemTemplate)
+//                                .param("documents", similarContext))
                 .advisors(a -> a.param(CONVERSATION_ID, username))
                 .user(message)
                 .call().content();

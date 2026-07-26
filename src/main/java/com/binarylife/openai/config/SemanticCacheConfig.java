ba926a1e318f5ee73b5e1600d2f7bd97ca65a637
Aritra Chatterjee <aritrachatterjee2007@gmail.com>
2026-07-26 07:35:48 +0530

    LLM028- apply semantic caching to rag processes using vector store instead of redis

--- src/main/java/com/binarylife/openai/config/SemanticCacheConfig.java
+++ src/main/java/com/binarylife/openai/config/SemanticCacheConfig.java
@@ -1,9 +1,13 @@
 package com.binarylife.openai.config;
 
+import io.qdrant.client.QdrantClient;
 import org.springframework.ai.chat.cache.semantic.SemanticCache;
 import org.springframework.ai.chat.cache.semantic.SemanticCacheAdvisor;
 import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.ai.vectorstore.qdrant.QdrantVectorStore;
 import org.springframework.ai.vectorstore.redis.cache.semantic.DefaultSemanticCache;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,25 +15,45 @@ import redis.clients.jedis.RedisClient;
 
 @Configuration
 public class SemanticCacheConfig {
+//    @Bean
+//    RedisClient redisClient(
+//            @Value("${spring.data.redis.host:localhost}")
+//            String host,
+//            @Value("${spring.data.redis.port:6379}")
+//            int port) {
+//        return RedisClient.builder().hostAndPort(host, port).build();
+//    }
+
     @Bean
-    RedisClient redisClient(
-            @Value("${spring.data.redis.host:localhost}")
-            String host,
-            @Value("${spring.data.redis.port:6379}")
-            int port) {
-        return RedisClient.builder().hostAndPort(host, port).build();
+    VectorStore cacheVectorStore(
+            QdrantClient qdrantClient,
+            EmbeddingModel embeddingModel
+    ) {
+        return QdrantVectorStore.builder(qdrantClient, embeddingModel)
+                .collectionName("binary-life-semantic-cache")
+                .initializeSchema(true)
+                .build();
     }
 
     @Bean
-    public SemanticCache semanticCache(RedisClient redisClient, EmbeddingModel embeddingModel) {
+    public SemanticCache semanticCache(@Qualifier("cacheVectorStore") VectorStore vectorStore, EmbeddingModel embeddingModel) {
         return DefaultSemanticCache.builder()
-                .jedisClient(redisClient)
+                .vectorStore(vectorStore)
                 .embeddingModel(embeddingModel)
-                .similarityThreshold(0.9)
-                .indexName("binary-life-semantic-cacehe")
-                .prefix("cache:")
+                .similarityThreshold(0.8)
                 .build();
     }
+
+//    @Bean
+//    public SemanticCache semanticCache(RedisClient redisClient, EmbeddingModel embeddingModel){
+//        return DefaultSemanticCache.builder()
+//                .jedisClient(redisClient)
+//                .embeddingModel(embeddingModel)
+//                .similarityThreshold(0.9)
+//                .indexName("binary-life-semantic-cache")
+//                .prefix("cache:")
+//                .build();
+//    }
 
     @Bean
     public SemanticCacheAdvisor semanticCacheAdvisor(SemanticCache semanticCache) {
